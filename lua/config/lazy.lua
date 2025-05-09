d4821007af6a688b5d931ca4bbe6e0fd276bc8d3
Daniel Graham <dnlgrhm@gmail.com>
2025-05-09 15:19:07 -0400

    Fixed addition of markdow-preview and render-markdow and added autolist.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,58 +1,3 @@
-----original
---local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
---if not (vim.uv or vim.loop).fs_stat(lazypath) then
---  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
---  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
---  if vim.v.shell_error ~= 0 then
---    vim.api.nvim_echo({
---      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
---      { out, "WarningMsg" },
---      { "\nPress any key to exit..." },
---    }, true, {})
---    vim.fn.getchar()
---    os.exit(1)
---  end
---end
---vim.opt.rtp:prepend(lazypath)
---
---require("lazy").setup({
---  spec = {
---    -- LazyVim core plugins
---    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
---
---    -- LazyVim extras
---    { import = "lazyvim.plugins.extras.coding.luasnip" },
---    { import = "lazyvim.plugins.extras.editor.aerial" },
---
---    -- Custom plugins
---    { "ellisonleao/gruvbox.nvim" },
---
---    -- Your own plugin folder
---    { import = "plugins" },
---  },
---  defaults = {
---    lazy = false,
---    version = false,
---  },
---  install = { colorscheme = { "gruvbox", "tokyonight", "habamax" } },
---  checker = {
---    enabled = true,
---    notify = false,
---  },
---  performance = {
---    rtp = {
---      disabled_plugins = {
---        "gzip",
---        "tarPlugin",
---        "tohtml",
---        "tutor",
---        "zipPlugin",
---      },
---    },
---  },
---})
---
--- new
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -71,24 +16,59 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
+    -- ✅ LazyVim core
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- ✅ LazyVim extras (MUST come after core)
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
+
+    -- ✅ Your custom plugins (MUST come after core + extras)
     { "ellisonleao/gruvbox.nvim" },
     {
       "LazyVim/LazyVim",
       opts = { colorscheme = "gruvbox" },
     },
-    -- ✅ explicitly include mason-lspconfig
-    -- { "williamboman/mason-lspconfig.nvim" },
     {
       "williamboman/mason-lspconfig.nvim",
       version = "1.29.0", -- known stable
     },
-    -- ✅ include fugitive (git tools)
     { "tpope/vim-fugitive" },
-    -- ✅ include lazygit if you like
     { "kdheepak/lazygit.nvim" },
+    {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      ft = { "markdown" },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+      opts = {},
+    },
+    {
+      "gaoDean/autolist.nvim",
+      ft = { "markdown", "text", "tex", "plaintex", "norg" },
+      config = function()
+        require("autolist").setup()
+
+        vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+        vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+        vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+        vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+        vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+        vim.keymap.set("n", "<C-x>", "<cmd>AutolistToggleCheckbox<cr>")
+        -- vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+        vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+
+        vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+        vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+
+        vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+      end,
+    },
   },
   defaults = { lazy = false, version = false },
   install = { colorscheme = { "gruvbox" } },
