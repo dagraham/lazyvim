629b08f17527dbd9f9fee04f5cf33b6b85cd68a3
Daniel Graham <dnlgrhm@gmail.com>
2025-05-13 18:00:56 -0400

    Daily backup from ~/ at 2025-05-13 18:00

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -14,5 +14,36 @@
 -- _G.doit = function(arg)
 --   vim.cmd("Doit " .. arg)
 -- end
--- require("config.colors")
-return {}
+require("config.colors")
+
+vim.api.nvim_create_user_command("AddProject", function(opts)
+  local project_name = opts.fargs[1]
+  local project_path = opts.fargs[2]
+  if not project_name or not project_path then
+    vim.notify("Usage: :AddProject <name> <directory>", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Expand tilde manually since jobstart doesn't use shell expansion
+  project_path = project_path:gsub("^~", os.getenv("HOME"))
+
+  local ok = vim.fn.executable("add_project.sh")
+  if ok == 0 then
+    vim.notify("add_project.sh not found in $PATH", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Run in background to avoid blocking
+  vim.fn.jobstart({ "add_project.sh", project_name, project_path }, {
+    detach = true,
+    on_exit = function(_, code)
+      if code ~= 0 then
+        vim.notify("add_project.sh exited with code " .. code, vim.log.levels.WARN)
+      end
+    end,
+  })
+end, {
+  nargs = "+",
+  complete = "file",
+  desc = "Add a new tmux project tab",
+})
