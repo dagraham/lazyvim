ff9bcbb0f065b14d66bade4ab474ff08b2ed8b1a
Daniel Graham <dnlgrhm@gmail.com>
2025-05-15 13:22:45 -0400

    "updates for folding"

--- lua/config/markdown_folds.lua
+++ lua/config/markdown_folds.lua
@@ -3,26 +3,26 @@ local M = {}
 
 function M.foldexpr()
   local line = vim.fn.getline(vim.v.lnum)
-  local prev_inside = vim.b._inside_codeblock or false
+  -- local prev_inside = vim.b._inside_codeblock or false
 
-  -- Start or end of fenced block
-  if line:match("^```") or line:match("^~~~") then
-    vim.b._inside_codeblock = not prev_inside
-    if not prev_inside then
-      return ">9" -- start of code block: fold level 9
-    else
-      return 0 -- end of code block
-    end
-  end
+  -- -- Start or end of fenced block
+  -- if line:match("^```") or line:match("^~~~") then
+  --   vim.b._inside_codeblock = not prev_inside
+  --   if not prev_inside then
+  --     return ">9" -- start of code block: fold level 9
+  --   else
+  --     return 0 -- end of code block
+  --   end
+  -- end
 
   -- Indented code: don't fold
   if line:match("^    ") or line:match("^\t") then
     return "="
   end
 
-  if vim.b._inside_codeblock then
-    return "9" -- inside code block: same fold level
-  end
+  -- if vim.b._inside_codeblock then
+  --  return "9" -- inside code block: same fold level
+  -- end
 
   -- Markdown heading
   local hashes = line:match("^(#+)")
