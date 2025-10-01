49f5f143a8ad359cc226ca71557ef0945c155720
Daniel Graham <dnlgrhm@gmail.com>
2025-09-30 23:59:56 -0400

    Daily backup from ~/.config/nvim at 2025-09-30 23:59

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,10 @@
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = false
-vim.opt.guifont = "MesloLGLDZ NF:h12"
+if vim.g.neovide then
+  vim.o.guifont = "MesloLGS Nerd Font:h12"
+end
+-- vim.opt.guifont = "MesloLGLDZ NF:h12"
 
 -- Folding for Treesitter-enabled files
 vim.o.foldmethod = "expr"
